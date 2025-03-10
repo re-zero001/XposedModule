a8f4b6270151e1a5097439f61b4aaca32c9be536
Irena <140869597+re-zero001@users.noreply.github.com>
2025-03-10 19:25:52 +0800

    Update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.7.2" apply false
+    id("com.android.application") version "8.9.0" apply false
     id("org.lsposed.lsplugin.resopt") version "1.6" apply false
     id("org.lsposed.lsparanoid") version "0.6.0" apply false
 }
